c505a9bf0efcde5517b250dfef5817a5db303471
aswathy <aswathipv8@gmail.com>
2019-08-20 16:14:48 +0530

    COMITTTTT

--- src/com/AUTO/Login.java
+++ src/com/AUTO/Login.java
@@ -1,3 +1,4 @@
+package com.AUTO;
 
 public class Login {
 
--- src/com/AUTO/login2.java
+++ src/com/AUTO/login2.java
@@ -1,9 +1,10 @@
+package com.AUTO;
 
-public class login3 {
+public class login2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("mmmmmmmmmmmmmmmm");
 	}
 
 }
